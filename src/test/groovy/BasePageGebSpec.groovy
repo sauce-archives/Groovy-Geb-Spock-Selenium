92d79e5b1bf33b5fadcdfcdd09729f183a998721
Titus Fortner <titusfortner@gmail.com>
2019-01-14 17:40:15 -0600

    start driver in before hook

--- src/test/groovy/BasePageGebSpec.groovy
+++ src/test/groovy/BasePageGebSpec.groovy
@@ -1,7 +1,58 @@
 import geb.driver.CachingDriverFactory
 import geb.spock.GebSpec
+import org.junit.Rule
+import org.junit.rules.TestName
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.MutableCapabilities
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.RemoteWebDriver
 
 class BasePageGebSpec extends GebSpec {
+    @Rule
+    public TestName name = new TestName() {
+        public String getMethodName() {
+            return super.getMethodName()
+        }
+    }
+
+    public String testDriver = System.getProperty('geb.env')
+
+    public void setup() throws Exception {
+        if (testDriver == "sauce") {
+            ChromeOptions chromeOpts = new ChromeOptions();
+            chromeOpts.setExperimentalOption("w3c", true);
+
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability(ChromeOptions.CAPABILITY, chromeOpts);
+            caps.setCapability("sauce:options", sauceCapabilities());
+            caps.setCapability("browserName", "googlechrome");
+            caps.setCapability("browserVersion", "71.0");
+            caps.setCapability("platformName", "windows 10");
+
+            URL url = new URL("https://ondemand.saucelabs.com:443/wd/hub");
+
+            driver = new RemoteWebDriver(url, caps);
+        } else {
+            driver = new ChromeDriver();
+        }
+    }
+
+    public MutableCapabilities sauceCapabilities() throws Exception {
+        String username = System.getenv("SAUCE_USERNAME");
+        String accessKey = System.getenv("SAUCE_ACCESS_KEY");
+        String methodName = name.getMethodName()
+
+        MutableCapabilities sauceOpts = new MutableCapabilities();
+        sauceOpts.setCapability("seleniumVersion", "3.141.1");
+        sauceOpts.setCapability("username", username);
+        sauceOpts.setCapability("accessKey", accessKey);
+        sauceOpts.setCapability("name", methodName);
+
+        return sauceOpts;
+    }
+
     @Override
     public void cleanup() throws Exception {
         driver.quit()
--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -16,58 +16,7 @@ waiting {
     timeout = 2
 }
 
-environments {
-    sauce {
-        driver = {
-            String username = System.getenv("SAUCE_USERNAME");
-            String accessKey = System.getenv("SAUCE_ACCESS_KEY");
-
-            ChromeOptions chromeOpts = new ChromeOptions();
-            chromeOpts.setExperimentalOption("w3c", true);
-
-            MutableCapabilities sauceOpts = new MutableCapabilities();
-            sauceOpts.setCapability("seleniumVersion", "3.141.1");
-            sauceOpts.setCapability("username", username);
-            sauceOpts.setCapability("accessKey", accessKey);
-
-            DesiredCapabilities caps = new DesiredCapabilities();
-            caps.setCapability(ChromeOptions.CAPABILITY, chromeOpts);
-            caps.setCapability("sauce:options", sauceOpts);
-            caps.setCapability("browserName", "googlechrome");
-            caps.setCapability("browserVersion", "71.0");
-            caps.setCapability("platformName", "windows 10");
-
-            URL url = new URL("https://ondemand.saucelabs.com:443/wd/hub")
-
-            new RemoteWebDriver(url, caps)
-        }
-    }
-
-    // run via “./gradlew chromeTest”
-    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
-    chrome {
-        driver = { new ChromeDriver() }
-    }
-
-    // run via “./gradlew chromeHeadlessTest”
-    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
-    chromeHeadless {
-        driver = {
-            ChromeOptions o = new ChromeOptions()
-            o.addArguments('headless')
-            new ChromeDriver(o)
-        }
-    }
-
-    // run via “./gradlew firefoxTest”
-    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-    firefox {
-        atCheckWaiting = 1
-
-        driver = { new FirefoxDriver() }
-    }
-
-}
+environments {}
 
 // To run the tests with all browsers just run “./gradlew test”
 
